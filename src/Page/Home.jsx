116d677f465f0ca038a99811eab3906427c07ecd
Reshma <reddyreshma85@gmai.com>
2024-05-22 13:16:45 -0400

    bug rendering

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home Page for the Portfolio</h1>
+    <div><h1>Check</h1>
+      <ul>
+        <li><Link to ="/">Home</Link></li>
+      
+
+        <li><Link to ="/AboutUs">About Us</Link></li>
+        <li><Link to ="/ContactMe">Home</Link>Contact Me</li>
+        <li><Link to ="/MyWork">Home</Link>My Work</li>
+      </ul>
+
+
     </div>
   );
 };
